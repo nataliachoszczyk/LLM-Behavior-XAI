c0e165f18caad189a547ec6dec676acc3e5ef94d
Dominika Boguszewska <dominikaboguszewska62@gmail.com>
2026-05-09 23:10:52 +0200

    Add encoding in read_prompts function

--- llm_response_analyzer/file_utils.py
+++ llm_response_analyzer/file_utils.py
@@ -8,7 +8,7 @@ def read_llm_results(path: str) -> TextFileReader | DataFrame:
 
 
 def read_prompts(path: str) -> TextFileReader | DataFrame:
-    return pd.read_csv(path, sep=";")
+    return pd.read_csv(path, sep=";", encoding="utf-8")
 
 
 def save_results(df: DataFrame, path: str) -> None:
